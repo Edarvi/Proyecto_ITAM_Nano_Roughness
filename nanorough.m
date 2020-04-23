d47626d07e3f097929f6e1de09da2ed1995b498a
Edarvi <61838489+Edarvi@users.noreply.github.com>
2020-04-22 20:25:38 -0500

    Agregué comentarios a nanorough.m
    Escribí comentarios describiendo las diferentes partes del codigo

--- nanorough.m
+++ nanorough.m
@@ -2,17 +2,26 @@ function nanorough
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
 
-    
+    % variable opciones va a guardar el nombre de las funciones que se
+    % pueden escoger.
+         %hay que cambiar el nombre de "opcion n" a el nombre
+         % de la funcion que queremos llamar (hasta ahora solo funciona
+         % como un nombre, no afecta el nombre aqui con el de las funciones
+         % reales
    opciones = {'opcion 1', 'opcion 2', 'opcion 3', ...
        'opcion 4', 'opcion 5', 'opcion 6', 'opcion 7', 'Salir'};
    
-   
+   % inicio de un ciclo para mostrar nuestro menu que terminara hasta que
+   % se escoga la opcion 8 que cambia x = false y termina el ciclo.
   x = true; 
    while x == true
        
-       choice = menu('Escoge una opción',opciones);
+       %este comando muestra el menu con todas sus opciones
+       choice = menu('Escoge una Funcion',opciones);
        
        
+       %lo que se ejecutará dependiendo de cada opción
+            %(Hace falta llenar esto)
        if choice == 1;
            fprintf('Escogió la opción #1 \n')
        
