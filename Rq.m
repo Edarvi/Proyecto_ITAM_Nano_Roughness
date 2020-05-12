be759d8b841fa2c21155c2824c3ef7013b195d57
benjagoni <64482153+benjagoni@users.noreply.github.com>
2020-05-12 13:15:00 -0500

    Update Rq.m

--- Rq.m
+++ Rq.m
@@ -1,5 +1,6 @@
 function[Rq] = Rq(Z) %Root Means Square Average of the profile is similar in the concept of linear regression
 	for i = 1:length(Z)
-		Rq = sqrt((1/length(Z))*(Z(i)-Zprom(Z)).^2);
+		Rq = sqrt((1/length(Z))*(Z(i)-Zprom(Z)).^2); %Donde Z es el roughness o datos de la imagen del AFM en una trayectoria determinda
+		% E "i" una variable para el procedimiento de la sumatoria
 	end
 end
