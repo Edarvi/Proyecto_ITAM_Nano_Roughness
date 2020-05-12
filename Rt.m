4d874c446da383a89d2cbbccc5323888f92ab117
benjagoni <64482153+benjagoni@users.noreply.github.com>
2020-05-12 12:55:54 -0500

    Create Rt.m

---
+++ Rt.m
@@ -0,0 +1,3 @@
+function[Rt] = Rt(Rp, Rv)%Devuelve la diferencia o rango entre el pico mas alto y el valle mas bajo 
+	Rt = Rp - Rv; %Donde Rt es igal al rango o diferencia, Rp el pico mas alto y Rv el valle mas bajo
+end
