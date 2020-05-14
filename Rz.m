4d1b87271bae50db79e367bec0d653f30351734c
Edarvi <61838489+Edarvi@users.noreply.github.com>
2020-05-14 10:16:07 -0500

    Correciones

--- Rz.m
+++
@@ -1,7 +0,0 @@
-function [Rz] = Rz(P, V)
-% average profile is the average found from de P greatest peaks
-% and V greatest valleys
-
-    Rz = (sum(P)+sum(V))/(length(P)+length(V));
-end
-
--- Zprom.m
+++
@@ -1,6 +0,0 @@
-function [Zprom] = Zprom(Z)
-%El promedio de los valores de las alturas
-
-Zprom = mean(Z);
-end
-
